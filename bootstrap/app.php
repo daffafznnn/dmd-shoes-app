8c8cff0f77819fcadbfdea6772b2dfead967036f
Rizal <rizalsofiana1976@gmail.com>
2024-12-19 19:11:14 +0700

    mendaftarkan middleware di app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'checkRole' => \App\Http\Middleware\checkRole::class,
+            'maintenance' => \App\Http\Middleware\maintenance::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
