33a6f01159e0ab1bf6bb5ad97ee61ce5dd182735
Rizal <rizalsofiana1976@gmail.com>
2024-12-28 20:41:29 +0700

    membuat page index product dan search product (statis)

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,10 @@ import Swal from 'sweetalert2';
 import Alpine from 'alpinejs';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel';
+
 window.Swal = Swal;
 window.Alpine = Alpine;
 
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.index');
 });
+Route::get('/products', function () {
+    return view('guest.product');
+})->name('products');
 
 Route::middleware('auth')->group(function () {
 
